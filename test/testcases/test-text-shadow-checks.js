3e9ae3c48c14c39fb9aa36c3e1d9bcfc8bff2893
Shane Stephens <shanestephens@google.com>
2013-04-18 10:48:10 +1000

    Last of the ref tests.

---
+++ test/testcases/test-text-shadow-checks.js
@@ -0,0 +1,7 @@
+
+check(document.querySelectorAll('body')[0],{'textShadow':'rgb(255, 0, 0) 100px 100px 0px, rgb(255, 0, 0) 0px 100px 10px'},0);
+check(document.querySelectorAll('body')[0],{'textShadow':'rgba(191, 0, 0, 0.74902) 75px 75px 0px, rgba(191, 0, 0, 0.74902) 0px 75px 7px'},0.5);
+check(document.querySelectorAll('body')[0],{'textShadow':'rgba(128, 0, 0, 0.498039) 50px 50px 0px, rgba(128, 0, 0, 0.498039) 0px 50px 5px'},1);
+check(document.querySelectorAll('body')[0],{'textShadow':'rgba(64, 0, 0, 0.247059) 25px 25px 0px, rgba(64, 0, 0, 0.247059) 0px 25px 2px'},1.5);
+check(document.querySelectorAll('body')[0],{'textShadow':'rgb(0, 0, 0) 0px 0px 0px'},2);
+check(document.querySelectorAll('body')[0],{'textShadow':'rgb(0, 0, 0) 0px 0px 0px'},2.5);
