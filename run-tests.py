87efbf789477ca2242fed73771eeb532deee49e6
Tim 'mithro' Ansell <mithro@mithis.com>
2013-04-22 16:12:39 +1000

    Removing screenshot upload as it doesn't work with parallel tests.

--- run-tests.py
+++ run-tests.py
@@ -36,11 +36,6 @@ parser.add_argument(
     "-v", "--verbose", action='store_true', default=False,
     help="Output more information.")
 
-parser.add_argument(
-    "-u", "--upload", action='store_true', default=False,
-    help="Upload images to picture sharing site (http://postimage.org/),"
-         " only really useful for testbots.")
-
 # Only used by the Remote browser option.
 parser.add_argument(
     "--remote-executor", type=str,
@@ -341,69 +336,6 @@ import mimetools
 import mimetypes
 import urllib, urllib2
 
-class MultiPartForm(object):
-    """Accumulate the data to be used when posting a form."""
-
-    def __init__(self):
-        self.form_fields = []
-        self.files = []
-        self.boundary = mimetools.choose_boundary()
-        return
-    
-    def get_content_type(self):
-        return 'multipart/form-data; boundary=%s' % self.boundary
-
-    def add_field(self, name, value):
-        """Add a simple field to the form data."""
-        self.form_fields.append((name, value))
-        return
-
-    def add_file(self, fieldname, filename, fileHandle, mimetype=None):
-        """Add a file to be uploaded."""
-        body = fileHandle.read()
-        if mimetype is None:
-            mimetype = mimetypes.guess_type(filename)[0] or 'application/octet-stream'
-        self.files.append((fieldname, filename, mimetype, body))
-        return
-    
-    def __str__(self):
-        """Return a string representing the form data, including attached files."""
-        # Build a list of lists, each containing "lines" of the
-        # request.  Each part is separated by a boundary string.
-        # Once the list is built, return a string where each
-        # line is separated by '\r\n'.  
-        parts = []
-        part_boundary = '--' + self.boundary
-        
-        # Add the form fields
-        parts.extend(
-            [ part_boundary,
-              'Content-Disposition: form-data; name="%s"' % name,
-              '',
-              value,
-            ]
-            for name, value in self.form_fields
-            )
-        
-        # Add the files to upload
-        parts.extend(
-            [ part_boundary,
-              'Content-Disposition: file; name="%s"; filename="%s"' % \
-                 (field_name, filename),
-              'Content-Type: %s' % content_type,
-              '',
-              body,
-            ]
-            for field_name, filename, content_type, body in self.files
-            )
-        
-        # Flatten the list and add closing boundary marker,
-        # then return CR+LF separated data
-        flattened = list(str(b) for b in itertools.chain(*parts))
-        flattened.append('--' + self.boundary + '--')
-        flattened.append('')
-        return '\r\n'.join(flattened)
-
 
 class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     STATUS = {0:'success', 1:'fail', 2:'fail', 3:'skip'}
@@ -437,25 +369,6 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
                 file_bytes=result['message'],
                 mime_type='text/plain; charset=UTF-8',
                 eof=True)
-
-        if overall_status > 0 and args.virtual:
-            screenshot = data['testName'] + '.png'
-            disp.grab().save(screenshot)
-
-            if args.upload:
-                form = MultiPartForm()
-                form.add_field('adult', 'no')
-                form.add_field('optsize', '0')
-                form.add_file('upload[]', screenshot, fileHandle=open(screenshot, 'rb'))
-
-                request = urllib2.Request("http://postimage.org/")
-                body = str(form)
-                request.add_header('Content-type', form.get_content_type())
-                request.add_header('Content-length', len(body))
-                request.add_data(body)
-
-                result = urllib2.urlopen(request).read()
-                print "Screenshot at:", re.findall("""<td><textarea wrap='off' onmouseover='this.focus\(\)' onfocus='this.select\(\)' id="code_1" scrolling="no">([^<]*)</textarea></td>""", result)
 
         SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
