77f2ea9f334a378ee9f33d7b79243e1881accbae
Tim 'mithro' Ansell <mithro@mithis.com>
2013-05-03 15:58:01 +1000

    Making testcase.js valid JSON.

--- test/update-testcases.sh
+++ test/update-testcases.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-(echo 'var tests = [' && (cd testcases && ls *.html) | sed "s/.*/  '\0',/" && echo '];') > testcases.js
+(echo 'var tests = [' && (cd testcases && ls *.html) | sed "s/.*/  \"\0\",/" && echo '];') > testcases.js
