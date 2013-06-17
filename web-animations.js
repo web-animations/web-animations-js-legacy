12513a339fc49ae398c6f0b82b7ba24681386a94
Shane Stephens <shanestephens@google.com>
2013-06-17 11:56:36 +1000

    new DOM methods.

--- web-animations.js
+++ web-animations.js
@@ -408,7 +408,7 @@ TimedItem.prototype = {
       this._player = null;
     }
     if (this.parentGroup !== null) {
-      this.parentGroup.remove(this.parentGroup.indexOf(this), 1);
+      this.remove();
     }
     this._parentGroup = parentGroup;
     // In the case of a SeqGroup parent, _startTime will be updated by
@@ -570,6 +570,30 @@ TimedItem.prototype = {
     throw new Error(
         "Derived classes must override TimedItem.clone()");
   },
+  before: function() {
+    var newItems = [];
+    for (var i = 0; i < arguments.length; i++) {
+      newItems.push(arguments[i]);
+    }
+    this.parentGroup._splice(this.parentGroup.indexOf(this), 0, newItems);
+  },
+  after: function() {
+    var newItems = [];
+    for (var i = 0; i < arguments.length; i++) {
+      newItems.push(arguments[i]);
+    }
+    this.parentGroup._splice(this.parentGroup.indexOf(this) + 1, 0, newItems);
+  },
+  replace: function() {
+    var newItems = [];
+    for (var i = 0; i < arguments.length; i++) {
+      newItems.push(arguments[i]);
+    }
+    this.parentGroup._splice(this.parentGroup.indexOf(this), 1, newItems);
+  },
+  remove: function() {
+    this.parentGroup._splice(this.parentGroup.indexOf(this), 1);
+  },
   // Gets the leaf TimedItems currently in effect. Note that this is a superset
   // of the leaf TimedItems in their active interval, as a TimedItem can have an
   // effect outside its active interval due to fill.
@@ -698,15 +722,13 @@ var TimingGroup = function(token, type, children, timing, parentGroup) {
   // used by TimedItem via _intrinsicDuration(), so needs to be set before
   // initializing super.
   this.type = type || 'par';
-  this.children = [];
+  this._children = [];
   this.length = 0;
   TimedItem.call(this, constructorToken, timing, parentGroup);
   // We add children after setting the parent. This means that if an ancestor
   // (including the parent) is specified as a child, it will be removed from our
   // ancestors and used as a child,
-  for (var i = 0; i < childrenCopy.length; i++) {
-    this.add(childrenCopy[i]);
-  }
+  this.append.apply(this, childrenCopy);
   // TODO: Work out where to expose name in the API
   // this.name = properties.name || '<anon>';
 };
@@ -761,6 +783,15 @@ TimingGroup.prototype = createObject(TimedItem.prototype, {
             child.animationDuration);
       }
     }
+  },
+  get children() {
+    return this._children;
+  },
+  get firstChild() {
+    return this.children[0];
+  },
+  get lastChild() {
+    return this.children[this.children.length - 1];
   },
   getAnimationsForElement: function(elem) {
     var result = [];
@@ -819,13 +850,19 @@ TimingGroup.prototype = createObject(TimedItem.prototype, {
   clear: function() {
     this.splice(0, this.children.length);
   },
-  add: function() {
+  append: function() {
     var newItems = [];
     for (var i = 0; i < arguments.length; i++) {
       newItems.push(arguments[i]);
     }
-    this.splice(this.length, 0, newItems);
-    return newItems;
+    this._splice(this.length, 0, newItems);
+  },
+  prepend: function() {
+    var newItems = [];
+    for (var i = 0; i < arguments.length; i++) {
+      newItems.push(arguments[i]);
+    }
+    this._splice(0, 0, newItems);
   },
   _addInternal: function(child) {
     this.children.push(child);
@@ -835,21 +872,15 @@ TimingGroup.prototype = createObject(TimedItem.prototype, {
   indexOf: function(item) {
     return this.children.indexOf(item);
   },
-  splice: function(start, deleteCount, newItems) {
+  _splice: function(start, deleteCount, newItems) {
     var args = arguments;
     if (args.length == 3) {
       args = [start, deleteCount].concat(newItems);
     }
     for (var i = 2; i < args.length; i++) {
       var newChild = args[i];
-      // Check whether the new child is an ancestor. If so, we need to break the
-      // chain immediately below the new child.
-      for (var ancestor = this; ancestor.parentGroup != null;
-          ancestor = ancestor.parentGroup) {
-        if (ancestor.parentGroup === newChild) {
-          newChild.remove(ancestor);
-          break;
-        }
+      if (this._isInclusiveAncestor(newChild)) {
+        throw new HierarchyRequestError();
       }
       newChild._reparent(this);
     }
@@ -861,11 +892,14 @@ TimingGroup.prototype = createObject(TimedItem.prototype, {
     this._childrenStateModified();
     return result;
   },
-  remove: function(index, count) {
-    if (!isDefined(count)) {
-      count = 1;
+  _isInclusiveAncestor: function(item) {
+    for (var ancestor = this; ancestor != null;
+      ancestor = ancestor.parentGroup) {
+      if (ancestor === item) {
+        return true;
+      }
     }
-    return this.splice(index, count);
+    return false;
   },
   toString: function() {
     return this.type + ' ' + this.startTime + '-' + this.endTime + ' (' +
