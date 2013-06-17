bed49c4258c51978f1b2aa56b3c32cc2a893e918
Alan Cutter <alancutter@chromium.org>
2013-06-17 16:28:11 +1000

    pants

--- tutorial/try-it-yourself.js
+++ tutorial/try-it-yourself.js
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+"use strict";
 
 // TryItDisplay creates and represents the input and output display.
  function TryItDisplay(containerElement) {
