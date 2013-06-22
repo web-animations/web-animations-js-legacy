c396628245783f370caed512233778778ffdb390
Douglas Stockwell <dstockwell@chromium.org>
2013-06-22 14:26:02 +1000

    Load web-animations.js in bootstrap

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -38,6 +38,8 @@ loadScript(location.pathname.replace('.html', '-checks.js'));
 document.write('<div id="log"></div>');
 loadScript('../testharness/testharnessreport.js');
 
+loadScript('../../web-animations.js');
+
 window.__coverage__ = parent.window.__coverage__;
 
 })();
