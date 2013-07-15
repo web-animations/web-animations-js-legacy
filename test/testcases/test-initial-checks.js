8da32be35b238ac04aca7ae135e1d1d3328b0c42
Alan Cutter <alancutter@chromium.org>
2013-07-15 14:35:34 +1000

    Split test element into smaller tests

--- test/testcases/test-initial-checks.js
+++ test/testcases/test-initial-checks.js
@@ -1,317 +1,74 @@
 timing_test(function() {
   at(0, function() {
-    assert_styles(".anim",{
-      'backgroundColor':'rgba(0, 0, 0, 0)',
-      'borderBottomColor':'rgb(0, 0, 0)',
-      'borderBottomLeftRadius':'0px',
-      'borderBottomRightRadius':'0px',
-      'borderBottomWidth':'3px',
-      'borderLeftColor':'rgb(0, 0, 0)',
-      'borderLeftWidth':'3px',
-      'borderRightColor':'rgb(0, 0, 0)',
-      'borderRightWidth':'3px',
-      'borderSpacing':'0px 0px',
-      'borderTopColor':'rgb(0, 0, 0)',
-      'borderTopLeftRadius':'0px',
-      'borderTopRightRadius':'0px',
-      'borderTopWidth':'3px',
-      'color':'rgb(0, 0, 0)',
-      'fontSize':'16px',
-      'fontWeight':'normal',
-      'letterSpacing':'normal',
-      'lineHeight':'20px',
-      'opacity':'1',
-      'outlineColor':'rgb(0, 0, 0)',
-      'outlineWidth':'3px',
-      'outlineOffset':'0px',
-      'paddingBottom':'0px',
-      'paddingLeft':'0px',
-      'paddingRight':'0px',
-      'paddingTop':'0px',
-      'textIndent':'0px',
-      'textShadow':'rgba(0, 0, 0, 0) 0px 0px 0px',
-      'transform':'matrix(1, 0, 0, 1, 0, 0)',
-      'verticalAlign':'0px',
-      'wordSpacing':'0px'});
-  });
-  at(0.5, function() {
-    assert_styles(".anim",{
-      'backgroundColor':'rgba(0, 0, 0, 0.121569)',
-      'borderBottomColor':'rgb(0, 32, 0)',
-      'borderBottomLeftRadius':'6.25px',
-      'borderBottomRightRadius':'6.25px',
-      'borderBottomWidth':'3px',
-      'borderLeftColor':'rgb(0, 32, 0)',
-      'borderLeftWidth':'3px',
-      'borderRightColor':'rgb(0, 32, 0)',
-      'borderRightWidth':'3px',
-      'borderSpacing':'6px 6px',
-      'borderTopColor':'rgb(0, 32, 0)',
-      'borderTopLeftRadius':'6.25px',
-      'borderTopRightRadius':'6.25px',
-      'borderTopWidth':'3px',
-      'color':'rgb(0, 16, 0)',
-      'fontSize':'18px',
-      'fontWeight':'normal',
-      'letterSpacing':'normal',
-      'lineHeight':'24px',
-      'opacity':'0.96875',
-      'outlineColor':'rgb(0, 16, 0)',
-      'outlineWidth':'3px',
-      'outlineOffset':'1px',
-      'paddingBottom':'6.25px',
-      'paddingLeft':'6.25px',
-      'paddingRight':'6.25px',
-      'paddingTop':'6.25px',
-      'textIndent':'6.25px',
-      'textShadow':'rgba(0, 32, 0, 0.121569) 1px 1px 6px',
-      'transform':'matrix(0.7071067811865476, 0.7071067811865475, -0.7071067811865475, 0.7071067811865476, 0, 0)',
-      'verticalAlign':'6.25px',
-      'wordSpacing':'12px'});
-  });
+    assert_styles("#background",{'backgroundColor':'rgba(0, 0, 0, 0)'});
+  }, "#background");
+  at(0, function() {
+    assert_styles("#border",{'borderBottomColor':'rgb(0, 0, 0)','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px','borderBottomWidth':'3px','borderLeftColor':'rgb(0, 0, 0)','borderLeftWidth':'3px','borderRightColor':'rgb(0, 0, 0)','borderRightWidth':'3px','borderTopColor':'rgb(0, 0, 0)','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderTopWidth':'3px'});
+  }, "#border");
+  at(0, function() {
+    assert_styles("#table",{'borderSpacing':'2px 2px','verticalAlign':'0px'});
+  }, "#table");
+  at(0, function() {
+    assert_styles("#opacity",{'opacity':'1'});
+  }, "#opacity");
+  at(0, function() {
+    assert_styles("#outline",{'outlineColor':'rgb(0, 0, 0)','outlineWidth':'3px','outlineOffset':'0px'});
+  }, "#outline");
+  at(0, function() {
+    assert_styles("#padding",{'paddingBottom':'0px','paddingLeft':'0px','paddingRight':'0px','paddingTop':'0px'});
+  }, "#padding");
+  at(0, function() {
+    assert_styles("#transform",{'transform':'matrix(1, 0, 0, 1, 0, 0)'});
+  }, "#transform");
+  at(0, function() {
+    assert_styles("#text",{'color':'rgb(0, 0, 0)','fontSize':'16px','fontWeight':'normal','letterSpacing':'normal','lineHeight':'20px','textIndent':'0px','textShadow':'rgba(0, 0, 0, 0) 0px 0px 0px','wordSpacing':'0px'});
+  }, "#text");
   at(1, function() {
-    assert_styles(".anim",{
-      'backgroundColor':'rgba(0, 0, 0, 0.247059)',
-      'borderBottomColor':'rgb(0, 64, 0)',
-      'borderBottomLeftRadius':'12.5px',
-      'borderBottomRightRadius':'12.5px',
-      'borderBottomWidth':'4px',
-      'borderLeftColor':'rgb(0, 64, 0)',
-      'borderLeftWidth':'4px',
-      'borderRightColor':'rgb(0, 64, 0)',
-      'borderRightWidth':'4px',
-      'borderSpacing':'12px 12px',
-      'borderTopColor':'rgb(0, 64, 0)',
-      'borderTopLeftRadius':'12.5px',
-      'borderTopRightRadius':'12.5px',
-      'borderTopWidth':'4px',
-      'color':'rgb(0, 32, 0)',
-      'fontSize':'20px',
-      'fontWeight':'500',
-      'letterSpacing':'1px',
-      'lineHeight':'29px',
-      'opacity':'0.9375',
-      'outlineColor':'rgb(0, 32, 0)',
-      'outlineWidth':'4px',
-      'outlineOffset':'2px',
-      'paddingBottom':'12.5px',
-      'paddingLeft':'12.5px',
-      'paddingRight':'12.5px',
-      'paddingTop':'12.5px',
-      'textIndent':'12.5px',
-      'textShadow':'rgba(0, 64, 0, 0.247059) 2px 2px 12px',
-      'transform':'matrix(0.00000000000000006123031769111886, 1, -1, 0.00000000000000006123031769111886, 0, 0)',
-      'verticalAlign':'12.5px',
-      'wordSpacing':'25px'});
-  });
-  at(1.5, function() {
-    assert_styles(".anim",{
-      'backgroundColor':'rgba(0, 0, 0, 0.372549)',
-      'borderBottomColor':'rgb(0, 96, 0)',
-      'borderBottomLeftRadius':'18.75px',
-      'borderBottomRightRadius':'18.75px',
-      'borderBottomWidth':'5px',
-      'borderLeftColor':'rgb(0, 96, 0)',
-      'borderLeftWidth':'5px',
-      'borderRightColor':'rgb(0, 96, 0)',
-      'borderRightWidth':'5px',
-      'borderSpacing':'18px 18px',
-      'borderTopColor':'rgb(0, 96, 0)',
-      'borderTopLeftRadius':'18.75px',
-      'borderTopRightRadius':'18.75px',
-      'borderTopWidth':'5px',
-      'color':'rgb(0, 48, 0)',
-      'fontSize':'21px',
-      'fontWeight':'500',
-      'letterSpacing':'1px',
-      'lineHeight':'32px',
-      'opacity':'0.90625',
-      'outlineColor':'rgb(0, 48, 0)',
-      'outlineWidth':'5px',
-      'outlineOffset':'3px',
-      'paddingBottom':'18.75px',
-      'paddingLeft':'18.75px',
-      'paddingRight':'18.75px',
-      'paddingTop':'18.75px',
-      'textIndent':'18.75px',
-      'textShadow':'rgba(0, 96, 0, 0.372549) 3px 3px 18px',
-      'transform':'matrix(-0.7071067811865475, 0.7071067811865476, -0.7071067811865476, -0.7071067811865475, 0, 0)',
-      'verticalAlign':'18.75px',
-      'wordSpacing':'37px'});
-  });
+    assert_styles("#background",{'backgroundColor':'rgba(0, 64, 0, 0.498039)'});
+  }, "#background");
+  at(1, function() {
+    assert_styles("#border",{'borderBottomColor':'rgb(0, 128, 0)','borderBottomLeftRadius':'25px','borderBottomRightRadius':'25px','borderBottomWidth':'6px','borderLeftColor':'rgb(0, 128, 0)','borderLeftWidth':'6px','borderRightColor':'rgb(0, 128, 0)','borderRightWidth':'6px','borderTopColor':'rgb(0, 128, 0)','borderTopLeftRadius':'25px','borderTopRightRadius':'25px','borderTopWidth':'6px'});
+  }, "#border");
+  at(1, function() {
+    assert_styles("#table",{'borderSpacing':'6px 6px','verticalAlign':'5px'});
+  }, "#table");
+  at(1, function() {
+    assert_styles("#opacity",{'opacity':'0.625'});
+  }, "#opacity");
+  at(1, function() {
+    assert_styles("#outline",{'outlineColor':'rgb(0, 64, 0)','outlineWidth':'6px','outlineOffset':'5px'});
+  }, "#outline");
+  at(1, function() {
+    assert_styles("#padding",{'paddingBottom':'25px','paddingLeft':'25px','paddingRight':'25px','paddingTop':'25px'});
+  }, "#padding");
+  at(1, function() {
+    assert_styles("#transform",{'transform':'matrix(-1, 0.00000000000000012246063538223773, -0.00000000000000012246063538223773, -1, 0, 0)'});
+  }, "#transform");
+  at(1, function() {
+    assert_styles("#text",{'color':'rgb(0, 64, 0)','fontSize':'23px','fontWeight':'bold','letterSpacing':'2px','lineHeight':'37px','textIndent':'25px','textShadow':'rgba(0, 128, 0, 0.498039) 5px 5px 25px','wordSpacing':'50px'});
+  }, "#text");
   at(2, function() {
-    assert_styles(".anim",{
-      'backgroundColor':'rgba(0, 0, 0, 0.498039)',
-      'borderBottomColor':'rgb(0, 128, 0)',
-      'borderBottomLeftRadius':'25px',
-      'borderBottomRightRadius':'25px',
-      'borderBottomWidth':'6px',
-      'borderLeftColor':'rgb(0, 128, 0)',
-      'borderLeftWidth':'6px',
-      'borderRightColor':'rgb(0, 128, 0)',
-      'borderRightWidth':'6px',
-      'borderSpacing':'25px 25px',
-      'borderTopColor':'rgb(0, 128, 0)',
-      'borderTopLeftRadius':'25px',
-      'borderTopRightRadius':'25px',
-      'borderTopWidth':'6px',
-      'color':'rgb(0, 64, 0)',
-      'fontSize':'23px',
-      'fontWeight':'600',
-      'letterSpacing':'2px',
-      'lineHeight':'37px',
-      'opacity':'0.875',
-      'outlineColor':'rgb(0, 64, 0)',
-      'outlineWidth':'6px',
-      'outlineOffset':'5px',
-      'paddingBottom':'25px',
-      'paddingLeft':'25px',
-      'paddingRight':'25px',
-      'paddingTop':'25px',
-      'textIndent':'25px',
-      'textShadow':'rgba(0, 128, 0, 0.498039) 5px 5px 25px',
-      'transform':'matrix(-1, 0.00000000000000012246063538223773, -0.00000000000000012246063538223773, -1, 0, 0)',
-      'verticalAlign':'25px',
-      'wordSpacing':'50px'});
-  });
-  at(2.5, function() {
-    assert_styles(".anim",{
-      'backgroundColor':'rgba(0, 0, 0, 0.623529)',
-      'borderBottomColor':'rgb(0, 159, 0)',
-      'borderBottomLeftRadius':'31.25px',
-      'borderBottomRightRadius':'31.25px',
-      'borderBottomWidth':'7px',
-      'borderLeftColor':'rgb(0, 159, 0)',
-      'borderLeftWidth':'7px',
-      'borderRightColor':'rgb(0, 159, 0)',
-      'borderRightWidth':'7px',
-      'borderSpacing':'31px 31px',
-      'borderTopColor':'rgb(0, 159, 0)',
-      'borderTopLeftRadius':'31.25px',
-      'borderTopRightRadius':'31.25px',
-      'borderTopWidth':'7px',
-      'color':'rgb(0, 80, 0)',
-      'fontSize':'25px',
-      'fontWeight':'600',
-      'letterSpacing':'3px',
-      'lineHeight':'43px',
-      'opacity':'0.84375',
-      'outlineColor':'rgb(0, 80, 0)',
-      'outlineWidth':'7px',
-      'outlineOffset':'6px',
-      'paddingBottom':'31.25px',
-      'paddingLeft':'31.25px',
-      'paddingRight':'31.25px',
-      'paddingTop':'31.25px',
-      'textIndent':'31.25px',
-      'textShadow':'rgba(0, 159, 0, 0.623529) 6px 6px 31px',
-      'transform':'matrix(-0.7071067811865477, -0.7071067811865475, 0.7071067811865475, -0.7071067811865477, 0, 0)',
-      'verticalAlign':'31.25px',
-      'wordSpacing':'62px'});
-  });
-  at(3, function() {
-    assert_styles(".anim",{
-      'backgroundColor':'rgba(0, 0, 0, 0.74902)',
-      'borderBottomColor':'rgb(0, 191, 0)',
-      'borderBottomLeftRadius':'37.5px',
-      'borderBottomRightRadius':'37.5px',
-      'borderBottomWidth':'8px',
-      'borderLeftColor':'rgb(0, 191, 0)',
-      'borderLeftWidth':'8px',
-      'borderRightColor':'rgb(0, 191, 0)',
-      'borderRightWidth':'8px',
-      'borderSpacing':'37px 37px',
-      'borderTopColor':'rgb(0, 191, 0)',
-      'borderTopLeftRadius':'37.5px',
-      'borderTopRightRadius':'37.5px',
-      'borderTopWidth':'8px',
-      'color':'rgb(0, 96, 0)',
-      'fontSize':'27px',
-      'fontWeight':'600',
-      'letterSpacing':'3px',
-      'lineHeight':'49px',
-      'opacity':'0.8125',
-      'outlineColor':'rgb(0, 96, 0)',
-      'outlineWidth':'8px',
-      'outlineOffset':'7px',
-      'paddingBottom':'37.5px',
-      'paddingLeft':'37.5px',
-      'paddingRight':'37.5px',
-      'paddingTop':'37.5px',
-      'textIndent':'37.5px',
-      'textShadow':'rgba(0, 191, 0, 0.74902) 7px 7px 37px',
-      'transform':'matrix(-0.0000000000000001836909530733566, -1, 1, -0.0000000000000001836909530733566, 0, 0)',
-      'verticalAlign':'37.5px',
-      'wordSpacing':'75px'});
-  });
-  at(3.5, function() {
-    assert_styles(".anim",{
-      'backgroundColor':'rgba(0, 0, 0, 0.87451)',
-      'borderBottomColor':'rgb(0, 223, 0)',
-      'borderBottomLeftRadius':'43.75px',
-      'borderBottomRightRadius':'43.75px',
-      'borderBottomWidth':'9px',
-      'borderLeftColor':'rgb(0, 223, 0)',
-      'borderLeftWidth':'9px',
-      'borderRightColor':'rgb(0, 223, 0)',
-      'borderRightWidth':'9px',
-      'borderSpacing':'43px 43px',
-      'borderTopColor':'rgb(0, 223, 0)',
-      'borderTopLeftRadius':'43.75px',
-      'borderTopRightRadius':'43.75px',
-      'borderTopWidth':'9px',
-      'color':'rgb(0, 112, 0)',
-      'fontSize':'28px',
-      'fontWeight':'bold',
-      'letterSpacing':'4px',
-      'lineHeight':'53px',
-      'opacity':'0.78125',
-      'outlineColor':'rgb(0, 112, 0)',
-      'outlineWidth':'9px',
-      'outlineOffset':'8px',
-      'paddingBottom':'43.75px',
-      'paddingLeft':'43.75px',
-      'paddingRight':'43.75px',
-      'paddingTop':'43.75px',
-      'textIndent':'43.75px',
-      'textShadow':'rgba(0, 223, 0, 0.87451) 8px 8px 43px',
-      'transform':'matrix(0.7071067811865474, -0.7071067811865477, 0.7071067811865477, 0.7071067811865474, 0, 0)',
-      'verticalAlign':'43.75px',
-      'wordSpacing':'87px'});
-  });
-  at(4, function() {
-    assert_styles(".anim",{
-      'backgroundColor':'rgb(0, 0, 0)',
-      'borderBottomColor':'rgb(0, 255, 0)',
-      'borderBottomLeftRadius':'50px',
-      'borderBottomRightRadius':'50px',
-      'borderBottomWidth':'10px',
-      'borderLeftColor':'rgb(0, 255, 0)',
-      'borderLeftWidth':'10px',
-      'borderRightColor':'rgb(0, 255, 0)',
-      'borderRightWidth':'10px',
-      'borderSpacing':'50px 50px',
-      'borderTopColor':'rgb(0, 255, 0)',
-      'borderTopLeftRadius':'50px',
-      'borderTopRightRadius':'50px',
-      'borderTopWidth':'10px',
-      'color':'rgb(0, 128, 0)',
-      'fontSize':'30px',
-      'fontWeight':'bold',
-      'letterSpacing':'5px',
-      'lineHeight':'60px',
-      'opacity':'0.75',
-      'outlineColor':'rgb(0, 128, 0)',
-      'outlineWidth':'10px',
-      'outlineOffset':'10px',
-      'paddingBottom':'50px',
-      'paddingLeft':'50px',
-      'paddingRight':'50px',
-      'paddingTop':'50px',
-      'textIndent':'50px',
-      'textShadow':'rgb(0, 255, 0) 10px 10px 50px',
-      'transform':'matrix(1, -0.00000000000000024492127076447545, 0.00000000000000024492127076447545, 1, 0, 0)',
-      'verticalAlign':'50px',
-      'wordSpacing':'100px'});
-  });
+    assert_styles("#background",{'backgroundColor':'rgb(0, 128, 0)'});
+  }, "#background");
+  at(2, function() {
+    assert_styles("#border",{'borderBottomColor':'rgb(0, 255, 0)','borderBottomLeftRadius':'50px','borderBottomRightRadius':'50px','borderBottomWidth':'10px','borderLeftColor':'rgb(0, 255, 0)','borderLeftWidth':'10px','borderRightColor':'rgb(0, 255, 0)','borderRightWidth':'10px','borderTopColor':'rgb(0, 255, 0)','borderTopLeftRadius':'50px','borderTopRightRadius':'50px','borderTopWidth':'10px'});
+  }, "#border");
+  at(2, function() {
+    assert_styles("#table",{'borderSpacing':'10px 10px','verticalAlign':'10px'});
+  }, "#table");
+  at(2, function() {
+    assert_styles("#opacity",{'opacity':'0.25'});
+  }, "#opacity");
+  at(2, function() {
+    assert_styles("#outline",{'outlineColor':'rgb(0, 128, 0)','outlineWidth':'10px','outlineOffset':'10px'});
+  }, "#outline");
+  at(2, function() {
+    assert_styles("#padding",{'paddingBottom':'50px','paddingLeft':'50px','paddingRight':'50px','paddingTop':'50px'});
+  }, "#padding");
+  at(2, function() {
+    assert_styles("#transform",{'transform':'matrix(1, -0.00000000000000024492127076447545, 0.00000000000000024492127076447545, 1, 0, 0)'});
+  }, "#transform");
+  at(2, function() {
+    assert_styles("#text",{'color':'rgb(0, 128, 0)','fontSize':'30px','fontWeight':'900','letterSpacing':'5px','lineHeight':'60px','textIndent':'50px','textShadow':'rgb(0, 255, 0) 10px 10px 50px','wordSpacing':'100px'});
+  }, "#text");
 }, "Auto generated tests");
