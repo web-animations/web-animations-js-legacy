7d00e247f34699def8a238d009466644ecc916f7
Alan Cutter <alancutter@chromium.org>
2013-07-31 20:50:44 +1000

    Rebase tests

--- test/testcases/auto-test-initial-checks.js
+++ test/testcases/auto-test-initial-checks.js
@@ -24,7 +24,7 @@ timing_test(function() {
     assert_styles("#text",{'color':'rgb(0, 0, 0)','fontSize':'13px','fontWeight':'normal','letterSpacing':'normal','lineHeight':'15px','textIndent':'0px','textShadow':'rgba(0, 0, 0, 0) 0px 0px 0px','wordSpacing':'0px'});
   }, "#text");
   at(1, function() {
-    assert_styles("#background",{'backgroundColor':'rgba(0, 64, 0, 0.498039)'});
+    assert_styles("#background",{'backgroundColor':'rgba(0, 128, 0, 0.498039)'});
   }, "#background");
   at(1, function() {
     assert_styles("#border",{'borderBottomColor':'rgb(0, 255, 0)','borderBottomLeftRadius':'25px','borderBottomRightRadius':'25px','borderBottomWidth':'6px','borderLeftColor':'rgb(0, 255, 0)','borderLeftWidth':'6px','borderRightColor':'rgb(0, 255, 0)','borderRightWidth':'6px','borderTopColor':'rgb(0, 255, 0)','borderTopLeftRadius':'25px','borderTopRightRadius':'25px','borderTopWidth':'6px'});
@@ -45,7 +45,7 @@ timing_test(function() {
     assert_styles("#transform",{'transform':'matrix(-1, 0.00000000000000012246063538223773, -0.00000000000000012246063538223773, -1, 0, 0)'});
   }, "#transform");
   at(1, function() {
-    assert_styles("#text",{'color':'rgb(0, 64, 0)','fontSize':'23px','fontWeight':'bold','letterSpacing':'5px','lineHeight':'36px','textIndent':'25px','textShadow':'rgba(0, 128, 0, 0.498039) 5px 5px 25px','wordSpacing':'100px'});
+    assert_styles("#text",{'color':'rgb(0, 64, 0)','fontSize':'23px','fontWeight':'bold','letterSpacing':'5px','lineHeight':'36px','textIndent':'25px','textShadow':'rgba(0, 255, 0, 0.498039) 5px 5px 25px','wordSpacing':'100px'});
   }, "#text");
   at(2, function() {
     assert_styles("#background",{'backgroundColor':'rgb(0, 128, 0)'});
