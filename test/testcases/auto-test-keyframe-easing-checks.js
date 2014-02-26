d5339fef977de6d59e4338ffe1c914e94fc5d5da
Alan Cutter <alancutter@chromium.org>
2014-02-26 15:12:05 +1100

    Simplified tests for Firefox compatibility

--- test/testcases/auto-test-keyframe-easing-checks.js
+++ test/testcases/auto-test-keyframe-easing-checks.js
@@ -3,315 +3,63 @@ timing_test(function() {
     assert_styles("#target",{'height':'0px'});
   });
   at(0.25, function() {
-    assert_styles("#target",{'height':'6.25px'});
-  });
-  at(0.5, function() {
     assert_styles("#target",{'height':'12.5px'});
   });
-  at(0.75, function() {
-    assert_styles("#target",{'height':'18.75px'});
-  });
-  at(1, function() {
+  at(0.5, function() {
     assert_styles("#target",{'height':'25px'});
   });
-  at(1.25, function() {
-    assert_styles("#target",{'height':'31.25px'});
-  });
-  at(1.5, function() {
+  at(0.75, function() {
     assert_styles("#target",{'height':'37.5px'});
   });
-  at(1.75, function() {
-    assert_styles("#target",{'height':'43.75px'});
-  });
-  at(2, function() {
+  at(1, function() {
     assert_styles("#target",{'height':'50px'});
   });
-  at(2.25, function() {
-    assert_styles("#target",{'height':'56.25px'});
-  });
-  at(2.5, function() {
+  at(1.25, function() {
     assert_styles("#target",{'height':'62.5px'});
   });
-  at(2.75, function() {
-    assert_styles("#target",{'height':'68.75px'});
-  });
-  at(3, function() {
+  at(1.5, function() {
     assert_styles("#target",{'height':'75px'});
   });
-  at(3.25, function() {
-    assert_styles("#target",{'height':'81.25px'});
-  });
-  at(3.5, function() {
+  at(1.75, function() {
     assert_styles("#target",{'height':'87.5px'});
   });
-  at(3.75, function() {
-    assert_styles("#target",{'height':'93.75px'});
-  });
-  at(4, function() {
+  at(2, function() {
     assert_styles("#target",{'height':'100px'});
   });
-  at(4.25, function() {
-    assert_styles("#target",{'height':'106.8px'});
+  at(2.25, function() {
+    assert_styles("#target",{'height':'106.5px'});
   });
-  at(4.5, function() {
-    assert_styles("#target",{'height':'120.4px'});
+  at(2.5, function() {
+    assert_styles("#target",{'height':'125px'});
   });
-  at(4.75, function() {
-    assert_styles("#target",{'height':'132.3px'});
+  at(2.75, function() {
+    assert_styles("#target",{'height':'143.5px'});
   });
-  at(5, function() {
-    assert_styles("#target",{'height':'140.1px'});
-  });
-  at(5.25, function() {
-    assert_styles("#target",{'height':'145.1px'});
-  });
-  at(5.5, function() {
-    assert_styles("#target",{'height':'148px'});
-  });
-  at(5.75, function() {
-    assert_styles("#target",{'height':'149.5px'});
-  });
-  at(6, function() {
+  at(3, function() {
     assert_styles("#target",{'height':'150px'});
   });
-  at(6.25, function() {
-    assert_styles("#target",{'height':'151.3px'});
+  at(3.25, function() {
+    assert_styles("#target",{'height':'150px'});
   });
-  at(6.5, function() {
-    assert_styles("#target",{'height':'154.7px'});
+  at(3.5, function() {
+    assert_styles("#target",{'height':'150px'});
   });
-  at(6.75, function() {
-    assert_styles("#target",{'height':'159.6px'});
-  });
-  at(7, function() {
-    assert_styles("#target",{'height':'165.8px'});
-  });
-  at(7.25, function() {
-    assert_styles("#target",{'height':'173px'});
-  });
-  at(7.5, function() {
-    assert_styles("#target",{'height':'181.1px'});
-  });
-  at(7.75, function() {
-    assert_styles("#target",{'height':'190.1px'});
-  });
-  at(8, function() {
+  at(3.75, function() {
     assert_styles("#target",{'height':'200px'});
   });
-  at(8.25, function() {
-    assert_styles("#target",{'height':'209.9px'});
+  at(4, function() {
+    assert_styles("#target",{'height':'200px'});
   });
-  at(8.5, function() {
-    assert_styles("#target",{'height':'218.9px'});
+  at(4.25, function() {
+    assert_styles("#target",{'height':'200px'});
   });
-  at(8.75, function() {
-    assert_styles("#target",{'height':'227px'});
+  at(4.5, function() {
+    assert_styles("#target",{'height':'225px'});
   });
-  at(9, function() {
-    assert_styles("#target",{'height':'234.2px'});
+  at(4.75, function() {
+    assert_styles("#target",{'height':'225px'});
   });
-  at(9.25, function() {
-    assert_styles("#target",{'height':'240.4px'});
-  });
-  at(9.5, function() {
-    assert_styles("#target",{'height':'245.3px'});
-  });
-  at(9.75, function() {
-    assert_styles("#target",{'height':'248.7px'});
-  });
-  at(10, function() {
+  at(5, function() {
     assert_styles("#target",{'height':'250px'});
-  });
-  at(10.25, function() {
-    assert_styles("#target",{'height':'251.5px'});
-  });
-  at(10.5, function() {
-    assert_styles("#target",{'height':'256.5px'});
-  });
-  at(10.75, function() {
-    assert_styles("#target",{'height':'264.6px'});
-  });
-  at(11, function() {
-    assert_styles("#target",{'height':'275px'});
-  });
-  at(11.25, function() {
-    assert_styles("#target",{'height':'285.4px'});
-  });
-  at(11.5, function() {
-    assert_styles("#target",{'height':'293.5px'});
-  });
-  at(11.75, function() {
-    assert_styles("#target",{'height':'298.4px'});
-  });
-  at(12, function() {
-    assert_styles("#target",{'height':'300px'});
-  });
-  at(12.25, function() {
-    assert_styles("#target",{'height':'285.9px'});
-  });
-  at(12.5, function() {
-    assert_styles("#target",{'height':'283.7px'});
-  });
-  at(12.75, function() {
-    assert_styles("#target",{'height':'297.1px'});
-  });
-  at(13, function() {
-    assert_styles("#target",{'height':'325px'});
-  });
-  at(13.25, function() {
-    assert_styles("#target",{'height':'352.9px'});
-  });
-  at(13.5, function() {
-    assert_styles("#target",{'height':'366.3px'});
-  });
-  at(13.75, function() {
-    assert_styles("#target",{'height':'364.1px'});
-  });
-  at(14, function() {
-    assert_styles("#target",{'height':'350px'});
-  });
-  at(14.25, function() {
-    assert_styles("#target",{'height':'366.7px'});
-  });
-  at(14.5, function() {
-    assert_styles("#target",{'height':'366.7px'});
-  });
-  at(14.75, function() {
-    assert_styles("#target",{'height':'383.3px'});
-  });
-  at(15, function() {
-    assert_styles("#target",{'height':'383.3px'});
-  });
-  at(15.25, function() {
-    assert_styles("#target",{'height':'383.3px'});
-  });
-  at(15.5, function() {
-    assert_styles("#target",{'height':'400px'});
-  });
-  at(15.75, function() {
-    assert_styles("#target",{'height':'400px'});
-  });
-  at(16, function() {
-    assert_styles("#target",{'height':'400px'});
-  });
-  at(16.25, function() {
-    assert_styles("#target",{'height':'400px'});
-  });
-  at(16.5, function() {
-    assert_styles("#target",{'height':'416.7px'});
-  });
-  at(16.75, function() {
-    assert_styles("#target",{'height':'416.7px'});
-  });
-  at(17, function() {
-    assert_styles("#target",{'height':'433.3px'});
-  });
-  at(17.25, function() {
-    assert_styles("#target",{'height':'433.3px'});
-  });
-  at(17.5, function() {
-    assert_styles("#target",{'height':'433.3px'});
-  });
-  at(17.75, function() {
-    assert_styles("#target",{'height':'450px'});
-  });
-  at(18, function() {
-    assert_styles("#target",{'height':'450px'});
-  });
-  at(18.25, function() {
-    assert_styles("#target",{'height':'450px'});
-  });
-  at(18.5, function() {
-    assert_styles("#target",{'height':'450px'});
-  });
-  at(18.75, function() {
-    assert_styles("#target",{'height':'466.7px'});
-  });
-  at(19, function() {
-    assert_styles("#target",{'height':'466.7px'});
-  });
-  at(19.25, function() {
-    assert_styles("#target",{'height':'466.7px'});
-  });
-  at(19.5, function() {
-    assert_styles("#target",{'height':'483.3px'});
-  });
-  at(19.75, function() {
-    assert_styles("#target",{'height':'483.3px'});
-  });
-  at(20, function() {
-    assert_styles("#target",{'height':'500px'});
-  });
-  at(20.25, function() {
-    assert_styles("#target",{'height':'550px'});
-  });
-  at(20.5, function() {
-    assert_styles("#target",{'height':'550px'});
-  });
-  at(20.75, function() {
-    assert_styles("#target",{'height':'550px'});
-  });
-  at(21, function() {
-    assert_styles("#target",{'height':'550px'});
-  });
-  at(21.25, function() {
-    assert_styles("#target",{'height':'550px'});
-  });
-  at(21.5, function() {
-    assert_styles("#target",{'height':'550px'});
-  });
-  at(21.75, function() {
-    assert_styles("#target",{'height':'550px'});
-  });
-  at(22, function() {
-    assert_styles("#target",{'height':'550px'});
-  });
-  at(22.25, function() {
-    assert_styles("#target",{'height':'550px'});
-  });
-  at(22.5, function() {
-    assert_styles("#target",{'height':'550px'});
-  });
-  at(22.75, function() {
-    assert_styles("#target",{'height':'550px'});
-  });
-  at(23, function() {
-    assert_styles("#target",{'height':'550px'});
-  });
-  at(23.25, function() {
-    assert_styles("#target",{'height':'600px'});
-  });
-  at(23.5, function() {
-    assert_styles("#target",{'height':'600px'});
-  });
-  at(23.75, function() {
-    assert_styles("#target",{'height':'600px'});
-  });
-  at(24, function() {
-    assert_styles("#target",{'height':'600px'});
-  });
-  at(24.25, function() {
-    assert_styles("#target",{'height':'600px'});
-  });
-  at(24.5, function() {
-    assert_styles("#target",{'height':'600px'});
-  });
-  at(24.75, function() {
-    assert_styles("#target",{'height':'600px'});
-  });
-  at(25, function() {
-    assert_styles("#target",{'height':'600px'});
-  });
-  at(25.25, function() {
-    assert_styles("#target",{'height':'600px'});
-  });
-  at(25.5, function() {
-    assert_styles("#target",{'height':'600px'});
-  });
-  at(25.75, function() {
-    assert_styles("#target",{'height':'600px'});
-  });
-  at(26, function() {
-    assert_styles("#target",{'height':'650px'});
   });
 }, "Auto generated tests");
