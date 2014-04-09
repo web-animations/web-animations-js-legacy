0a9af98479d6298f5d6d73935080c3981f253e3d
Alan Cutter <alancutter@chromium.org>
2014-04-09 20:21:54 +1000

    Use nicer dimensions for testing

--- test/testcases/auto-test-transform-origin-checks.js
+++ test/testcases/auto-test-transform-origin-checks.js
@@ -1,37 +1,37 @@
 timing_test(function() {
   at(0, function() {
-    assert_styles(".test",{'transformOrigin':'53px 53px'});
+    assert_styles(".test",{'webkitTransformOrigin':'50px 50px'});
   });
   at(1, function() {
     assert_styles(".test", [
-      {'transformOrigin':'53px 53px'},
-      {'transformOrigin':'26.5px 53px'},
-      {'transformOrigin':'79.5px 53px'},
-      {'transformOrigin':'53px 26.5px'},
-      {'transformOrigin':'53px 79.5px'},
-      {'transformOrigin':'39px 53px'},
-      {'transformOrigin':'26.5px 26.5px'},
-      {'transformOrigin':'31.8px 51.5px'},
-      {'transformOrigin':'31.8px 51.5px 50px'},
-      {'transformOrigin':'79.5px 79.5px 50px'},
-      {'transformOrigin':'79.5px 40.09px'},
-      {'transformOrigin':'79.5px 18.89px -100px'},
+      {'webkitTransformOrigin':'50px 50px'},
+      {'webkitTransformOrigin':'25px 50px'},
+      {'webkitTransformOrigin':'75px 50px'},
+      {'webkitTransformOrigin':'50px 25px'},
+      {'webkitTransformOrigin':'50px 75px'},
+      {'webkitTransformOrigin':'37.5px 50px'},
+      {'webkitTransformOrigin':'25px 25px'},
+      {'webkitTransformOrigin':'30px 50px'},
+      {'webkitTransformOrigin':'30px 50px 50px'},
+      {'webkitTransformOrigin':'75px 75px 50px'},
+      {'webkitTransformOrigin':'75px 35px'},
+      {'webkitTransformOrigin':'75px 35px -100px'},
     ]);
   });
   at(2, function() {
     assert_styles(".test", [
-      {'transformOrigin':'53px 53px'},
-      {'transformOrigin':'0px 53px'},
-      {'transformOrigin':'106px 53px'},
-      {'transformOrigin':'53px 0px'},
-      {'transformOrigin':'53px 106px'},
-      {'transformOrigin':'25px 53px'},
-      {'transformOrigin':'0px 0px'},
-      {'transformOrigin':'10.59px 50px'},
-      {'transformOrigin':'10.59px 50px 100px'},
-      {'transformOrigin':'106px 106px 100px'},
-      {'transformOrigin':'106px 27.19px'},
-      {'transformOrigin':'106px -15.19px -200px'},
+      {'webkitTransformOrigin':'50px 50px'},
+      {'webkitTransformOrigin':'0px 50px'},
+      {'webkitTransformOrigin':'100px 50px'},
+      {'webkitTransformOrigin':'50px 0px'},
+      {'webkitTransformOrigin':'50px 100px'},
+      {'webkitTransformOrigin':'25px 50px'},
+      {'webkitTransformOrigin':'0px 0px'},
+      {'webkitTransformOrigin':'10px 50px'},
+      {'webkitTransformOrigin':'10px 50px 100px'},
+      {'webkitTransformOrigin':'100px 100px 100px'},
+      {'webkitTransformOrigin':'100px 20px'},
+      {'webkitTransformOrigin':'100px 20px -200px'},
     ]);
   });
 }, "Auto generated tests");
